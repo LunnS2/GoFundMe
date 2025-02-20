c76376c23586d6aa4c41d4812aaedfe3001937a7
Lunn <thafm.work@gmail.com>
2025-02-20 18:02:41 +0000

    Add animation to svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,19 @@ function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href={"http://google.com"}>
-          <div>
+        <Link
+          href="http://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="group relative inline-block"
+        >
+          <div className="relative">
             <Image
               src="/GoFundMe-WebsiteIMG-Final-6c-NW.svg"
-              width={500}
-              height={500}
+              width={800}
+              height={800}
               alt="Picture of the author"
+              className="transition-transform duration-300 ease-in-out transform group-hover:scale-105"
             />
           </div>
         </Link>
