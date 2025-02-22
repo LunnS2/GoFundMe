8d69df7826a1251f57bdc82031966697fc3254a0
Lunn <thafm.work@gmail.com>
2025-02-22 21:23:25 +0000

    Update NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,12 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { MenuIcon, X } from "lucide-react";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname(); // Get current path
 
   return (
     <nav className="bg-quartary text-secondary p-2 fixed w-full top-0 left-0 z-50">
@@ -15,11 +17,7 @@ function NavBar() {
           onClick={() => setIsOpen(!isOpen)}
           className="lg:hidden text-secondary focus:outline-none transition-colors duration-300"
         >
-          {isOpen ? (
-            <X className="w-6 h-6" />
-          ) : (
-            <MenuIcon className="w-6 h-6" />
-          )}
+          {isOpen ? <X className="w-6 h-6" /> : <MenuIcon className="w-6 h-6" />}
         </button>
 
         {/* Centered Links for Large Screens */}
@@ -28,10 +26,22 @@ function NavBar() {
             isOpen ? "flex flex-col space-y-4" : "hidden"
           } lg:block mx-auto`}
         >
-          <Link href="/">Welcome</Link>
-          <Link href="/updates">Updates</Link>
-          <Link href="/who-i-am">Who I Am</Link>
-          <Link href="/get-in-touch">Get In Touch</Link>
+          {[
+            { href: "/", label: "Welcome" },
+            { href: "/updates", label: "Updates" },
+            { href: "/who-i-am", label: "Who I Am" },
+            { href: "/get-in-touch", label: "Get In Touch" },
+          ].map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`hover:text-black ${
+                pathname === link.href ? "text-black font-semibold"  : ""
+              }`}
+            >
+              {link.label}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
